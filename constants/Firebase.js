896d38157ac1f2a70c4d0a4ea082836b7767bf77
Kevin Lin <kevinlin505@hotmail.com>
2018-06-08 15:41:11 -0400

    Check gitignore

--- constants/Firebase.js
+++ constants/Firebase.js
@@ -1,8 +1,8 @@
 export default {
-  apiKey: '',
-  authDomain: '',
-  databaseURL: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: ''
+  apiKey: 'AIzaSyCI0Y-Eypm0b3IHONj3rHzTM4kV37CYwco',
+  authDomain: 'doit-90d46.firebaseapp.com',
+  databaseURL: 'https://doit-90d46.firebaseio.com',
+  projectId: 'doit-90d46',
+  storageBucket: 'doit-90d46.appspot.com',
+  messagingSenderId: '918750318336'
 };
